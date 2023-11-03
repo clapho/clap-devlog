2e7471ec5de9d6b75e4579bb184dbde827dd8e8b
SoohoPark <clapp301@gmail.com>
2023-11-03 15:18:20 +0900

    chore: 프로젝트 생성

---
+++ src/main/java/com/example/clapdevlog/ClapDevlogApplication.java
@@ -0,0 +1,13 @@
+package com.example.clapdevlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClapDevlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClapDevlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/clapdevlog/ClapDevlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clapdevlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClapDevlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
